a7a14ca727ebcbd07c4ba0b7a303037e82f8916a
Shohag Mia <shohag.mia@datasoft-bd.com>
2019-05-23 16:14:08 +0600

    Initial

---
+++ UrlCustomize/Areas/Blog/BlogAreaRegistration.cs
@@ -0,0 +1,37 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace UrlCustomize.Areas.Blog
+{
+    public class BlogAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "Blog";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+
+            context.MapRoute(
+                "Blog_Post",
+                "Blog/{controller}/{action}/{date}/{serial}/{article}",
+                new { action = "Index", date = UrlParameter.Optional, serial = UrlParameter.Optional, article = UrlParameter.Optional },
+                new
+                {
+                    date = @"^[0-9] $",
+                    serial = @"^[0-9] $",
+                    method = new HttpMethodConstraint("Get")
+                });
+
+            context.MapRoute(
+                "Blog_default",
+                "Blog/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ UrlCustomize/Areas/Blog/Controllers/AzureController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.Ajax.Utilities;
+
+namespace UrlCustomize.Areas.Blog.Controllers
+{
+    public class AzureController : Controller
+    {
+        // GET: Blog/Azure
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Blog/Azure/DevOps/
+        // GET: Blog/Azure/DevOps/2019/
+        // GET: Blog/Azure/DevOps/2019/1001/
+        // GET: Blog/Azure/DevOps/2019/1001/Introductions/
+        // GET: Blog/Azure/DevOps?date=2019&serial=1001&article=AspNetMvc
+
+        [HttpGet]
+        public ActionResult DevOps(string date, string serial, string article)
+        {
+
+            if (!date.IsNullOrWhiteSpace() && !serial.IsNullOrWhiteSpace() && !article.IsNullOrWhiteSpace())
+            {
+                ViewBag.Message = "Date-" + date + ", Serial-" + serial + " & Article-" + article;
+                return View();
+            }
+            if (!date.IsNullOrWhiteSpace() && !serial.IsNullOrWhiteSpace())
+            {
+                ViewBag.Message = "Date-" + date + ", Serial-" + serial;
+                return View();
+            }
+            if (!date.IsNullOrWhiteSpace())
+            {
+                ViewBag.Message = "Date-" + date;
+                return View();
+            }
+
+            return View();
+        }
+    }
+}
